3ff745450f35557cfe599b8175524610f93905ae
Aman Agnihotri <you@example.com>
2022-09-19 12:54:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const scrape=require("./controller/scrape");
+const cors = require('cors');
+const app=express();
+app.use(cors());
+app.use("/:user",scrape);
+app.get("/",(req,res)=>
+{
+    res.send("API is Working Successfully");
+})
+app.listen(process.env.PORT|| 5000,()=>
+{
+    console.log("backend server is up at port:3000");
+})
